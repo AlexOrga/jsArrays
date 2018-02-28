fbaa89fed4e25390d587f72a1614e880e1c2740c
Alex Orga <alexorga.dev@gmail.com>
2018-02-27 19:46:41 -0600

    completed challenge 1

--- arrays.js
+++ arrays.js
@@ -1,1 +1,22 @@
 console.log('Hi');
+
+// Challenge 1
+
+var test1 = 'cat';
+var test2 = 'racecar';
+var test3 = 'bear';
+var test4 = 'mom';
+var tests = [test1, test2, test3, test4];
+var i;
+
+for (i = 0; i < tests.length; i += 1) {
+    var string = tests[i];
+    var splitString=string.split('');
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join('');
+    if (string === joinArray){
+         console.log('true');
+    } else {
+         console.log('false');
+ }
+}
