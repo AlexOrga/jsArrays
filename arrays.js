45214702dcf69a461ce859e5fe88d8fe2d23d4bc
Alex Orga <alexorga.dev@gmail.com>
2018-02-27 21:18:42 -0600

    completed exercises 1 and 2 in arrays.js and empty div in html

--- arrays.js
+++ arrays.js
@@ -20,3 +20,34 @@ for (i = 0; i < tests.length; i += 1) {
          console.log('false');
  }
 }
+
+// Exercise
+
+var animals = ['fish', 'dog', 'cat'];
+var animalsElement = document.getElementById("animals");
+for(var i=0; i<animals.length; i++){
+    animalsElement.innerHTML += '<h5>' + animals[i] + '</h5>';
+}
+
+// Challenge 2
+
+// var book1 = "Catch 22";
+// var book2 = "Slaughterhouse 5";
+// var book3 = "Fahrenheit 451";
+// var bookArray = [book1, book2, book3];
+
+// for(i=0; i<bookArray.length; i++){
+//     var stringSplit = bookArray[i].split(' ').pop();
+//     console.log(stringSplit);
+// }
+
+var book = "catch 22";
+var bookSplit = book.split('');
+var realNumz = [];
+
+for(var j=0; j<bookSplit.length; j++){
+    if( (bookSplit[j]*1)){
+        realNumz.push(bookSplit[j]);
+    }
+}
+console.log("realNumz:", realNumz.join(''));
